9776ad418b7b529732267d586d59042ecec43198
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2025-08-29 16:44:16 -0300

    :sparkles: Limpeza dos projetos e adição do header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1 className="header-title">
+        Hi! My name is Jhonatan and <br/>
+        I’m a Back End <br/>
+        Developer
+      </h1>
+    </div>
+  )
+}
